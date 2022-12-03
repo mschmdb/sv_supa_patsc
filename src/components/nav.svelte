64a3081db5e58e522fc42699791e41d4e667c2a4
mschmdb <113426956+mschmdb@users.noreply.github.com>
2022-12-03 04:44:31 +0100

    npm i daisyui

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,3 +1,3 @@
 <nav class="border-b dark:border-gray-700">
-<div class="m-8"><a href="/" class="underline">client side</a>&nbsp;|&nbsp<a href="/ssr" class="underline">server side</a>&nbsp;|&nbsp<a href="/map" class="underline">graph with D9</a></div>
+<div class="m-8"><a  href="/" class="underline" data-sveltekit-prefetch>client side</a>&nbsp;|&nbsp<a href="/ssr" data-sveltekit-prefetch class="underline">server side</a>&nbsp;|&nbsp<a href="/map" class="underline" data-sveltekit-prefetch>graph with D9</a></div>
 </nav>
